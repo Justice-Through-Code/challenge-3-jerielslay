d62aea5f0a35ac9baae5791db9e51e160c5aa36a
Jeriel Slay <SlayJ@protonmail.com>
2022-12-04 21:17:05 -0500

    updated

--- stocks.py
+++ stocks.py
@@ -1,21 +1,24 @@
 
 from unittest import result
 
-
 def stock_purchases():
-    amazon = 3000
-    apple = 100
-    fb = 250
-    google = 1400
-    msft = 200
+    stocks = {
+    "amazon" : 3000,
+    "apple" : 100,
+    "fb" : 250,
+    "google" : 1400,
+    "msft" : 200
+    }
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-    client = input('What is your name? ')
+    client_name = input("What is your name ")
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
     # and save it into a variable
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
-    dollars = int(input('How much would you like to invest? '))
+    dollars =int(input("How much would you like to invest? $")) 
+    # dollars = "How much would you like to invest! $"
+    #doolars = int(dollars)
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
     stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
@@ -25,26 +28,24 @@ def stock_purchases():
 
     # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
-    if stock_name == "Amazon" or stock_name == "amazon":
-        stock_amt = dollars // amazon
-        stock = amazon
-    if stock_name == "Apple" or stock_name == "apple":
-        stock_amt = dollars // apple
-        stock = apple
-    if stock_name == "Facebook" or stock_name == "facebook":
-        stock_amt = dollars // fb
-        stock = fb
-    if stock_name == "Google" or stock_name == "google":
-        stock_amt = dollars // google
-        stock = google
-    if stock_name == "Microsoft" or stock_name == "microsoft":
-        stock_amt = dollars // msft
-        stock = msft
-
+    howMuchStock = 0
+    priceOfStock = 0
+    if stock_name == "Amazon" : 
+        howMuchStock =int( dollars/stocks["amazon"])
+        priceOfStock = stocks["amazon"]
+    elif stock_name == "Apple":  
+        howMuchStock =int( dollars/stocks["apple"])
+        priceOfStock = stocks["apple"]
+    elif stock_name == "Facebook" : 
+        howMuchStock = int(dollars/stocks["fb"])
+        priceOfStock = stocks["fb"]
+    elif stock_name == "Google" : 
+        howMuchStock = int(dollars/stocks["google"])  
+        priceOfStock = stocks["google"]
+    elif stock_name == "Microsoft" : 
+        howMuchStock = int(dollars/stocks["msft"]) 
+        priceOfStock = stocks["msft"]
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
-    def shares():
-        return stock_amt 
-    
-    print(f'{client} has ${dollars} to invest and can buy {stock_amt} shares of {stock_name} at the current price of ${stock}.')
+    print(f"{client_name} has ${dollars} to invest and can buy {howMuchStock} shares of {stock_name} at the current price of ${priceOfStock}.")
